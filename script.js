56b12c622f5aa6227a2c9d3112b359e4bcea546f
Marcos Galvan <marcosgalvan2476@gmail.com>
2023-07-09 06:37:49 -0400

    adding something in

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
+
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
 	let results = [];
-
-	// TODO
-
+	 const fruits = fruit.filter(food => food.includes(str));
+	if(fruits.toLowerCase().includes(str) || fruits.toUpperCase().includes(str) === true){
+		return fruits += results;
+	}
 	return results;
-}
+};
 
 function searchHandler(e) {
+	e.preventDefault();
+	input.value=''
+
+}
+
+function showSuggestions(results, ...inputVal) {
+
 	// TODO
 }
 
-function showSuggestions(results, inputVal) {
+suggestions.addEventListener('click', useSuggestion); {
+	useSuggestion.preventDefault();
+	if (useSuggestion.target.tagName === 'LI') {
+		input.value='';
+		
 
-	// TODO
-}
-
-function useSuggestion(e) {
-	// TODO
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
