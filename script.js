5d72dfd912700123e474f7e4fc030a2dce0d227b
Marcos Galvan <marcosgalvan2476@gmail.com>
2023-07-21 11:34:50 -0400

    adding work to JS

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
+const results = function search(str) {
 	let results = [];
 	 const fruits = fruit.filter(food => food.includes(str));
-	if(fruits.toLowerCase().includes(str) || fruits.toUpperCase().includes(str) === true){
-		return fruits += results;
-	}
+	// if(fruits.toLowerCase().includes(str) || fruits.toUpperCase().includes(str) === true){
+	// 	return fruits += results;
+	// }
+	let foundIndex = fruits.findIndex(string => string.toLowerCase() === 1);
+
+	if (fruits.toLowerCase() === foundIndex){
+		return fruits += results
+	};
 	return results;
 };
 
 function searchHandler(e) {
-	// TODO
+	e.preventDeafult();
+	input.innerText = suggestions.value; 
+	suggestions.appendChild(results);
 };
 
-function showSuggestions(results, ...inputVal) {
+function showSuggestions(results, inputVal) {
 
 };
 
