69accefed09a3e9f3f1f1d6603abf811955ca3e8
Marcos Galvan <marcosgalvan2476@gmail.com>
2023-07-23 03:29:27 -0400

    fixing up code

--- script.js
+++ script.js
@@ -4,37 +4,35 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-const results = function search(str) {
+  function search(str) {
 	let results = [];
-	 const fruits = fruit.filter(food => food.includes(str));
-	// if(fruits.toLowerCase().includes(str) || fruits.toUpperCase().includes(str) === true){
-	// 	return fruits += results;
-	// }
-	let foundIndex = fruits.findIndex(string => string.toLowerCase() === 1);
-
-	if (fruits.toLowerCase() === foundIndex){
+	 const fruits = fruit.filter(food => food.toLowerCase().includes(str.toLowerCase()));
 		return fruits += results
 	};
 	return results;
-};
 
+
+	// WHAT I'M DOING FOR searchHandler IS MAKING A LI SO I CAN APPEND THE FILTERED FRUIT ARRAY TO IT AND APPEND IT TO THE SUGGESTIONS UL 
 function searchHandler(e) {
 	e.preventDeafult();
-	input.innerText = suggestions.value; 
-	suggestions.appendChild(results);
+	const fruitList = document.createElement('li');
+	const result = search(e.currentTarget.value);
+	fruitList.append(result);
+	suggestions.append(fruitList);
+	
 };
 
+
+// loop through the results and for each suggestion append to suggestions ul and 'results' is the result of that fruit that I'm returning 
 function showSuggestions(results, inputVal) {
 
 };
 
+
+//  create a function that empty's out the suggestions field and use the event listener that listens for clicks to replace the input.value.
 function useSuggestion(e) {
 	// TODO
 };
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
-// useSuggestion.preventDefault();
-// 	if (useSuggestion.target.tagName === 'LI') {
-// 		input.value='';
-// 		input.value= useSuggestion;
